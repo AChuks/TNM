80d44d3029e6bffa7d67a7c5c6e9c79bab465875
Abraham Chukwuka <abraham@r65labs.com>
2016-12-27 15:43:39 -0500

    removed prod assets in public dir

--- app/assets/javascripts/owl_carousel.js
+++ app/assets/javascripts/owl_carousel.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 	var owl = $(".owl-carousel");
 	if (initialLoad == true){
-		owl.html(owl.find('.owl-carousel').html());
+		owl.html(owl.find('.owl-wrapper').html());
 	}
 	owl.owlCarousel({
     	autoPlay: true
