5acca976ec7d53ff173ac6fbc032f94b956f1611
Abraham Chukwuka <abraham@r65labs.com>
2016-12-28 15:22:37 -0500

    removed Tuber theme images/renamed app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
     def full_title(page_title)
-      base_title = "Afro Renaissance "
+      base_title = "TNM"
       if page_title.empty?
         base_title
       else
