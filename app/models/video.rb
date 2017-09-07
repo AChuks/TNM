012e8b32fc8212b6f26aeea3f752a37115017a69
AChuks <abrahamchukwuka@gmail.com>
2017-09-07 17:10:27 -0400

    added validation to video model and video submission form

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ActiveRecord::Base
-
+	validates :author, presence: true, length: {maximum: 50}
+	validates :title, presence: true, length: {maximum: 50}
+	validates :description, presence: true, length: {maximum: 250}
+	validates :url, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 end
