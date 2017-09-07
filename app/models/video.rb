32107c256c09d870293765b9b666504ddf117ca7
AChuks <abrahamchukwuka@gmail.com>
2017-09-07 17:19:24 -0400

    removed unused email regex from video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,5 +3,5 @@ class Video < ActiveRecord::Base
 	validates :title, presence: true, length: {maximum: 50}
 	validates :description, presence: true, length: {maximum: 250}
 	validates :url, presence: true
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 end
