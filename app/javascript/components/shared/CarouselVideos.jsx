5093c013f64e04eea24fbb7576fe80d26164491f
Abraham Chukwuka <abrahamchukwuka@gmail.com>
2019-12-02 20:04:00 -0500

    use max resolution thumbnail images

--- app/javascript/components/shared/CarouselVideos.jsx
+++ app/javascript/components/shared/CarouselVideos.jsx
@@ -30,7 +30,7 @@ class CarouselVideos extends Component {
                   <LazyLoadImage
                     alt={`${carouselVideo.title}`}
                     height="auto"
-                    src={`https://i.ytimg.com/vi/${carouselVideo.url}/mqdefault.jpg`}
+                    src={`https://i.ytimg.com/vi/${carouselVideo.url}/maxresdefault.jpg`}
                     width="100%"
                   />
                   <div className="video-title">{carouselVideo.title}</div>
