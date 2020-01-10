0c52fac244b54c22c48b5a65c6f8d5f09834bd4c
Abraham Chukwuka <abrahamchukwuka@gmail.com>
2020-01-10 00:05:37 -0500

    updated featured video

--- app/javascript/components/HomeComponent.jsx
+++ app/javascript/components/HomeComponent.jsx
@@ -120,7 +120,7 @@ class HomeComponent extends Component {
                   className="featured-video"
                   width="1200"
                   height={featuredVideoHeight}
-                  src="https://www.youtube.com/embed/t6Xc8JWyo_4"
+                  src="https://www.youtube.com/embed/Rv6nwold6ig"
                   frameBorder="0"
                   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
