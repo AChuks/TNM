1a07c22906011cc3f500f9c5fa4602d9af8704d8
Abraham Chukwuka <abrahamchukwka@gmail.com>
2020-04-03 11:05:13 -0400

    updated featured video

--- app/javascript/components/HomeComponent.jsx
+++ app/javascript/components/HomeComponent.jsx
@@ -113,7 +113,7 @@ class HomeComponent extends Component {
                   controls
                   width="1200"
                   height={featuredVideoHeight}
-                  data-setup='{ "techOrder": ["youtube", "html5"], "sources": [{ "type": "video/youtube", "src": "https://www.youtube.com/watch?v=BxfB2KINBSg"}] }'
+                  data-setup='{ "techOrder": ["youtube", "html5"], "sources": [{ "type": "video/youtube", "src": "https://www.youtube.com/watch?v=N0t9W0WOsco"}] }'
                 ></video>
                 {/* <iframe
                   className="featured-video"
