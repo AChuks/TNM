a66ad3e75ad08dbc3d75eb9bbed19b6d6fc7d4ab
Abraham Chukwuka <abrahamchukwka@gmail.com>
2020-04-11 18:42:56 -0400

    changed port to 3000

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ worker_timeout 100000
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3333
+port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
