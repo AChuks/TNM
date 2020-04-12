4fcc3330c15436ceb45170a833d9880c4521ab7c
Abraham Chukwuka <abrahamchukwka@gmail.com>
2020-04-12 16:36:30 -0400

    more config changes

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,8 @@
-bind "unix:///var/run/puma/my_app.sock"
-pidfile "/var/run/puma/my_app.sock"
 stdout_redirect "/var/log/puma.log", "/var/log/puma.log", true
 
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-port        ENV['PORT']     || 3000
-environment ENV['RAILS_ENV'] || 'development'
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RACK_ENV") { "development" }
 plugin :tmp_restart
