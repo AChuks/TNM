b4331ba12e24a6562d34e1730e265728c097ffdb
Abraham Chukwuka <abrahamchukwka@gmail.com>
2020-04-18 17:31:56 -0400

    modified puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,7 @@
-bind "unix:///var/run/puma/my_app.sock"
-pidfile "/var/run/puma/my_app.sock"
-
+if ENV.fetch("RAILS_ENV") == 'production'
+    bind "unix:///var/run/puma/my_app.sock"
+    pidfile "/var/run/puma/my_app.sock"
+end
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
