f7c07ef27aa0851f0e32856d44ef518f329d7866
Abraham Chukwuka <abrahamchukwka@gmail.com>
2020-04-28 23:24:04 -0400

    debugging redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 REDIS_CONFIG = YAML::load_file(Rails.root.join('config', 'redis.yml'))
-$redis = Redis.new(:host => REDIS_CONFIG['host'], :port => REDIS_CONFIG['port'])
+$redis = Redis.new(:host => 'cache-supercomedian-dev.8s6hwq.ng.0001.euw1.cache.amazonaws.com', :port => 6379)
