ab6c8e1e25b876fcb9c1aba94ee3e0f16a528e1c
Abraham Chukwuka <abrahamchukwka@gmail.com>
2020-06-01 16:15:54 -0400

    updated featured video

--- app/javascript/components/HomeComponent.jsx
+++ app/javascript/components/HomeComponent.jsx
@@ -65,8 +65,8 @@ class HomeComponent extends Component {
   };
 
   handleFeaturedVideoClick = ()  => {
-    // window.location.href = '/watch?url=9odfgTp3gPQ&irl=true';
-    window.location.href = '/watch?url=y3hythCCbh8&meta_data=UCkKHhORmLc14fmYvOa4Y7Ug';
+    window.location.href = '/watch?url=R9Gd0FX3qXc&irl=true';
+    // window.location.href = '/watch?url=y3hythCCbh8&meta_data=UCkKHhORmLc14fmYvOa4Y7Ug';
   }
 
   scrollToTopOfVideoGrid = () => {
@@ -110,8 +110,8 @@ class HomeComponent extends Component {
           <div className="col-xs-12" onClick={this.handleFeaturedVideoClick}>
             <div>
               <div className="col-xs-12">
-                <img src='https://img.youtube.com/vi/y3hythCCbh8/mqdefault.jpg' style={{height: 'inherit', width: 'inherit', cursor: 'pointer'}}/>
-                <h1 className="video-title">KRAKSTERS PRANKED WITH COCOA POWDER | Kruising With Kraksters Ep 1</h1>
+                <img src='https://img.youtube.com/vi/R9Gd0FX3qXc/mqdefault.jpg' style={{height: 'inherit', width: 'inherit', cursor: 'pointer'}}/>
+                <h1 className="video-title">Female Pharmacist Shows Man How to Use Condom</h1>
                 {/* <video
                   id="featureVideo"
                   className="video-js vjs-default-skin featured-video"
