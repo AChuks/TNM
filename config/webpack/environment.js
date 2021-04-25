b616d51bff037294faf30a1c891529551f8613de
Abraham Chukwuka <abrahamchukwuka@gmail.com>
2021-04-25 15:24:25 -0400

    debugging

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,2 @@
 const {environment} = require('@rails/webpacker')
-environment.config.merge({
-    devtool: 'none'
-})
 module.exports = environment
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,8 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const webpackConfig = require('./base')
-webpackConfig.config.merge({
-    devtool: 'none'
-})
 
 module.exports = webpackConfig
